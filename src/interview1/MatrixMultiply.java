ac9d9f65329b209585d78d93b53db3c62c72cda2
tanu <24976766+tanusgit@users.noreply.github.com>
2020-04-05 18:46:12 -0700

    matrix

--- src/interview1/MatrixMultiply.java
+++ src/interview1/MatrixMultiply.java
@@ -9,7 +9,14 @@ public class MatrixMultiply {
 	}
 
 	private static int[][] matrixMultiply(int[][] a, int[][] b) {
-		
+		int res = 0;
+		for(int i = 0;i < a.length; i++) {
+			for(int j = 0; j < b.length; j++) {
+				for(int k = 0; k < a.length; k++) {
+					a[i][k] = a[i][k] * b[k][i] ;
+				}
+			}
+		}
 		return null;
 	}
 }
