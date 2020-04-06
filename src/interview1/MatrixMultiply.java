58e0e8de1c31d546e01cfa43478964c1ad8c60b5
tanu <24976766+tanusgit@users.noreply.github.com>
2020-04-05 18:49:57 -0700

    changed matrix

--- src/interview1/MatrixMultiply.java
+++ src/interview1/MatrixMultiply.java
@@ -5,18 +5,21 @@ public class MatrixMultiply {
 		// 2 by 2 array
 		int[][] a = { { 1, 6 }, { 3, 5 } };
 		int[][] b = { { 2, 7 }, { 4, 9 } };
-		int[][] c = matrixMultiply(a, b);
+		matrixMultiply(a, b);
 	}
 
-	private static int[][] matrixMultiply(int[][] a, int[][] b) {
+	private static void matrixMultiply(int[][] a, int[][] b) {
+		int c[][]=new int[a.length][b.length];  
 		int res = 0;
 		for(int i = 0;i < a.length; i++) {
 			for(int j = 0; j < b.length; j++) {
+				c[i][j]=0;   
 				for(int k = 0; k < a.length; k++) {
-					a[i][k] = a[i][k] * b[k][i] ;
+					c[i][j]+=a[i][k]*b[k][j];      
 				}
+				System.out.print(c[i][j]+" "); 
 			}
+			System.out.println();
 		}
-		return null;
 	}
 }
