caddace6e73a688717ea655f1ddfc28ea52a746f
tanu <24976766+tanusgit@users.noreply.github.com>
2020-04-14 20:30:05 -0700

    found

--- src/interview1/FindThreeNum.java
+++ src/interview1/FindThreeNum.java
@@ -1,29 +1,47 @@
 package interview1;
 
 import java.util.ArrayList;
-
+//find three num whose sum is closest to the target num
 public class FindThreeNum {
 	public static void main(String[] args) {
-		int array[] = { 1, 2, 5, 4, 5, 6, 8, 9 };
-		int target = 5;
+		int array[] = { 1, 2, 5, 4, 6, 8, 9 };
+		int target = 15;
 		find(array, target);
 	}
 
 	private static void find(int[] array, int target) {
 		int sum = 0;
+		int minDiff = 0;
+
 		ArrayList<Keep> array2 = new ArrayList<>();
+		ArrayList<Integer> array3 = new ArrayList<>();
 		for (int i = 0; i < array.length; i++) {
 			for (int j = i + 1; j < array.length; j++) {
 				for (int k = j + 1; k < array.length; k++) {
 					sum = array[i] + array[j] + array[k];
-					Keep e = new Keep(array[i], array[j], array[k], sum);
-					if (Math.abs(sum-target) == 1) {
-						array2.add(e);
-					}
+					minDiff = Math.abs(sum - target);
+					Keep e = new Keep(array[i], array[j], array[k], sum, minDiff);
+					array2.add(e);
+					array3.add(minDiff);
 				}
 			}
 		}
-		System.out.println(array2);
+
+		// System.out.println(array2);
+		// System.out.println(array3);
+		int min = array3.get(0);
+		for (int l = 0; l < array3.size(); l++) {
+			if (array3.get(l) < min) {
+				min = array3.get(l);
+			}
+
+		}
+		// System.out.println(min);
+		for (int n = 0; n < array2.size(); n++) {
+			if (min == array2.get(n).min) {
+				System.out.println(array2.get(n).a + " " + array2.get(n).b + " " + array2.get(n).c);
+			}
+		}
 	}
 
 }
@@ -33,13 +51,15 @@ class Keep {
 	int b;
 	int c;
 	int sum;
+	int min;
 
-	public Keep(int a, int b, int c, int sum) {
+	public Keep(int a, int b, int c, int sum, int min) {
 		super();
 		this.a = a;
 		this.b = b;
 		this.c = c;
 		this.sum = sum;
+		this.min = min;
 	}
 
 	public int getA() {
@@ -68,7 +88,7 @@ class Keep {
 
 	@Override
 	public String toString() {
-		return "Keep [a=" + a + ", b=" + b + ", c=" + c + ", sum=" + sum + "]";
+		return "Keep [a=" + a + ", b=" + b + ", c=" + c + ", sum=" + sum + " min=" + min + "]";
 	}
 
 }
