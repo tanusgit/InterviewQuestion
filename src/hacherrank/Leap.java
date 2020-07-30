32705914b7686bbde3ef8a91a8803c9a193eafb7
tanu <24976766+tanusgit@users.noreply.github.com>
2020-07-30 14:09:58 -0700

    leap game program

--- src/hacherrank/Leap.java
+++ src/hacherrank/Leap.java
@@ -2,51 +2,42 @@ package hacherrank;
 
 public class Leap {
 	public static void main(String args[]) {
-		int array[] = {1, 0, 1, 0, 0};
-		int leap = 3;
+		int array[] = {0,1,0};
+		int leap = 1;
 		String m = win(array, leap);
 		System.out.println(m);
-		int i=0;
-		while( i < 5) {
-			System.out.println(i);
-			i++;
+		int num = ones(array);
+		//System.out.println("result = " + num);
+	}
+	
+	
+	public static int ones(int array[]) {
+		int count = 1;
+		for(int i = 0; i < array.length-1; i++) {
+			if(array[i] == 1 && array[i+1] == 1) {
+				count++;
+				//System.out.println("inside if = " + count);
+			}
 		}
+		return count;
 	}
 
 	public static String win(int array[], int leap) {
+		int n = ones(array);
 		String res = "";
-		int len = array.length;
-		int k = 0;
+		
 		boolean flag = false;
-		int temp = 0;
-		while(k < len-1) {
-			System.out.println("at the beginning  " + k);
-			if(array[k+1] == 0 ) {
-			System.out.println("inside first if" + (k+1) + " " + array[k+1]);
-				k++;
-				temp = k;
-				flag = true;
-			//	System.out.println("this is temp" + temp);
-			}
-	
-			if(temp+leap < len && array[temp+leap] == 0) {
-			//	System.out.println(temp + " +" + leap+ " " + (temp+leap));
-				flag = true;
-			}
-			else {
-				//System.out.println("failed");
-				flag = false;
-			}
-			
-			System.out.println("at the end  " + k);
-			k++;
-			
+		
+		if(leap > n) {
+			flag = true;
 		}
-		//System.out.println(flag);
+		
 		if(flag == true) {
 			res = "win";
 		} 
-		else res = "failed";
+		else {
+			res = "failed";
+		}
 		
 
 		return res;
