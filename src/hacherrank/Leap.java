9a8de606b8461e30769cc1637531c5c1292ef366
tanu <24976766+tanusgit@users.noreply.github.com>
2020-07-30 19:32:46 -0700

    leap game program

--- src/hacherrank/Leap.java
+++ src/hacherrank/Leap.java
@@ -2,43 +2,56 @@ package hacherrank;
 
 public class Leap {
 	public static void main(String args[]) {
-		int array[] = {0,1,0};
+		int array[] = { 0, 1, 1, 1, 0, 1, 1, 1, 0 };
 		int leap = 1;
 		String m = win(array, leap);
 		System.out.println(m);
 		int num = ones(array);
-		//System.out.println("result = " + num);
+		System.out.println("result = " + num);
 	}
-	
-	
+
+	// counting the consecutive one
 	public static int ones(int array[]) {
-		int count = 1;
-		for(int i = 0; i < array.length-1; i++) {
-			if(array[i] == 1 && array[i+1] == 1) {
+		int count = 0;
+		int newcount = 0;
+		for (int i = 0; i < array.length; i++) {
+			
+			if (array[i]==0) {
+				count = 0;
+			/*if (array[i] == 1 && array[i + 1] == 1) {
 				count++;
-				//System.out.println("inside if = " + count);
+
+				if (array[i] == 0) {
+					count = 1;
+				}
+*/
+				// System.out.println("inside if = " + count);
 			}
+			else {
+				count++;
+			}
+			//newcount is storing the max array of ones in each iteration of the loop
+			newcount = Math.max(newcount, count);
+
 		}
-		return count;
+		return newcount;
 	}
 
 	public static String win(int array[], int leap) {
 		int n = ones(array);
 		String res = "";
-		
+
 		boolean flag = false;
-		
-		if(leap > n) {
+
+		if (leap > n) {
 			flag = true;
 		}
-		
-		if(flag == true) {
+
+		if (flag == true) {
 			res = "win";
-		} 
-		else {
+		} else {
 			res = "failed";
 		}
-		
 
 		return res;
 	}
