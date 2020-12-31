67453f2a2a9a0b2ec6c02a3633343cc6c8100013
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-12-31 15:53:35 -0800

    Add files via upload

--- Range.java
+++ Range.java
@@ -1,42 +1,85 @@
 package dataStructures;
 
-import java.util.ArrayList;
-
-public class Range {
-	public static void main(String[] args) {
-		int[][] array = {
-				{1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9 ,10, 11, 12}
-            };
-		ArrayList m = findSmallestRange(array, 5, 3);
-		System.out.println(m);
-		
-		int smallest = (int) m.get(0);
-		System.out.println(m.size());
-		int n = m.size();
-		System.out.println("smallest before " + smallest);
-		for(int i =0; i < n; i++) {
-			if(smallest > (int)m.get(i)) {
-				smallest = (int)m.get(i);
-			}
-		}
-		System.out.println(smallest);
-		
-		
-	}
-
-	static ArrayList<Integer> findSmallestRange(int[][] arr, int n, int k) {
-		ArrayList<Integer> a = new ArrayList<>();
-		int smallest = arr[0][0];
-		for (int i = 0; i < k; i++) {
-			
-			a.add(arr[i][0]);
-			}
-			
-			System.out.println(" ");
-		
-
-		return a;
-	}
-}
+class Range{ 
+  
+    static final int N = 5; 
+  
+    // array for storing the current index of list i 
+    static int ptr[] = new int[501]; 
+  
+    // This function takes an k sorted lists in the form of 
+    // 2D array as an argument. It finds out smallest range 
+    // that includes elements from each of the k lists. 
+    static void findSmallestRange(int arr[][], int n, int k) 
+    { 
+        int i, minval, maxval, minrange, minel = 0, maxel = 0, flag, minind; 
+  
+        // initializing to 0 index; 
+        for (i = 0; i <= k; i++) { 
+            ptr[i] = 0; 
+        } //ptr[1] = 0; ptr[2]=0; ptr[3]=0;
+  
+        minrange = Integer.MAX_VALUE; 
+  
+        while (true) { 
+            // for mainting the index of list containing the minimum element 
+            minind = -1; 
+            minval = Integer.MAX_VALUE; 
+            maxval = Integer.MIN_VALUE; 
+            flag = 0; 
+  
+            // iterating over all the list 
+            for (i = 0; i < k; i++) { 
+                // if every element of list[i] is traversed then break the loop 
+            	System.out.println("printing  ptr[i]  = "+ ptr[i] + " and n = "+ n );
+                if (ptr[i] == n) { 
+                    flag = 1; 
+                    break; 
+                } 
+                // find minimum value among all the list elements pointing by the ptr[] array 
+                if (ptr[i] < n && arr[i][ptr[i]] < minval) { 
+                    minind = i; // update the index of the list 
+                    minval = arr[i][ptr[i]]; 
+                    System.out.println("printing inside minval arr[i][ptr[i]] = "+ arr[i][ptr[i]]);
+                } 
+                // find maximum value among all the list elements pointing by the ptr[] array 
+                if (ptr[i] < n && arr[i][ptr[i]] > maxval) { 
+                    maxval = arr[i][ptr[i]]; 
+                    System.out.println("printing  inside maxval arr[i][ptr[i]] = "+ arr[i][ptr[i]]);
+                } 
+            }
+            System.out.println(" ");
+            System.out.println("after for loop");
+            System.out.println(" ");
+            // if any list exhaust we will not get any better answer, so break the while loop 
+            if (flag == 1) { 
+                break; 
+            } 
+  
+            ptr[minind]++; 
+  
+            // updating the minrange 
+            if ((maxval - minval) < minrange) { 
+                minel = minval; 
+                maxel = maxval; 
+                minrange = maxel - minel; 
+            } 
+        } 
+        System.out.printf("The smallest range is [%d, %d]\n", minel, maxel); 
+    } 
+  
+    // Driver program to test above function 
+    public static void main(String[] args) 
+    { 
+  
+        int arr[][] = { 
+            { 4, 7, 9, 12, 15 }, 
+            { 0, 8, 10, 14, 20 }, 
+            { 6, 12, 16, 30, 50 } 
+        }; 
+  
+        int k = arr.length; 
+  
+        findSmallestRange(arr, N, k); 
+    } 
+} 
