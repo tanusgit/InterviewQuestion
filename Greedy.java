16c4f6f1de255e174d04be4582d376a2f1d66b4b
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-02-25 08:55:49 -0800

    mergesort

--- Greedy.java
+++ Greedy.java
@@ -4,10 +4,22 @@ public class Greedy {
     public static void main(String[] args) {
         //largest num
         int m = 123489;
-        int arr[] = {1, 2, 3, 4, 8, 9};
+        int arr[] = {1, 2, 3, 4, 8, 9, 0, 5, 6, 1, 2, 5};
         char c[] = {'1', '2', '3', '8', '9'};
-        int res2 = largestNum(c);
-        System.out.print(res2);
+       // int res2 = largestNum(c);
+       // System.out.print(res2);
+        /*bubbleSort(arr);
+        for (int i: arr
+             ) {
+            System.out.print(i);
+        }*/
+
+       mergeSort(arr, 0, 11);
+       // merge(arr, 0, 5, 11);
+        for (int i: arr
+        ) {
+           System.out.println("after mergesort " + i);
+        }
 
     }
 
@@ -38,4 +50,94 @@ public class Greedy {
         ch[zero] = '0';
         return max;
     }
+
+
+    public static void bubbleSort(int[] a ){
+        for(int i = 0; i < a.length-1; i++){
+            for(int j = i+1; j < a.length; j++){
+                if(a[i] > a[j]){
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void mergeSort(int[] a, int left, int right ){
+            if(left < right){
+                int mid = left + (right - left) / 2;
+                mergeSort(a, left, mid);
+                mergeSort(a, mid+1, right);
+                merge(a, left, mid, right);
+            }
+    }
+
+    private static void merge(int[] a, int left, int mid, int right) {
+        int leftlen = (mid - left )+1;
+        int rightlen = (right - mid);
+        int[] l = new int[leftlen];
+        int[] r = new int[rightlen];
+        for(int i = 0; i < leftlen; i++){
+            l[i] = a[i+left];
+        }
+        for(int j = 0; j < rightlen; j++){
+            r[j] = a[j+mid+1];
+        }
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while(i < leftlen && j < rightlen){
+            if(l[i] <= r[j]){
+                a[k] = l[i];
+                i++;
+                k++;
+            }else{
+                a[k] = r[j];
+                j++;
+                k++;
+            }
+        }
+        while(i < leftlen){
+            a[k] = l[i];
+            i++;
+            k++;
+        }
+        while(j < rightlen){
+            a[k] = r[j];
+            j++;
+            k++;
+        }
+
+    }
+
+
+    public static void merge(int[] a , int[] b){
+        int len = a.length;
+        int len2 = b.length;
+        int c[] = new int[len+len2];
+        int i = 0;
+        int j = 0;
+
+        while(i < len && j < len2){
+            if(a[i] < b[j]){
+                c[i] = a[i];
+                i++;
+            }else{
+                c[j] = b[j];
+                j++;
+            }
+        }
+        while(i < len){
+            c[i] = a[i];
+            i++;
+        }
+        while(j < len2){
+            c[j] = b[j];
+            j++;
+        }
+    }
+
+
 }
