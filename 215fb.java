799066402f1d602d5855b6173a5ad21dffccef4a
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-02-28 12:58:10 -0800

    Update 215fb.java

--- 215fb.java
+++ 215fb.java
@@ -11,3 +11,32 @@ Note that it is the kth largest element in the sorted order, not the kth distinc
         return nums[nums.length-k];
     }
 }
+
+
+//using selection sort insetead of javas inbuilt sort function
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        //selection sort to sort the array
+        
+       // Arrays.sort(nums);
+        selectionsort(nums);
+        return nums[nums.length-k];
+    }
+    
+    public void selectionsort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            int min = i;
+            for(int j = i+1; j < arr.length; j ++){
+                if(arr[min]> arr[j]){
+                    min = j;
+                }
+            }
+            if(min != i){
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+}
