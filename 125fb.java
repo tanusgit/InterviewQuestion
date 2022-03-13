9d2fce13ed3280489b658a81a2afa2233607ea34
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-03-13 10:40:22 -0700

    Update 125fb.java

--- 125fb.java
+++ 125fb.java
@@ -5,21 +5,17 @@ class Solution {
         char sp = ' ';
         for(int i = 0; i < arr.length; i++){
             char c = arr[i]; 
-            if(Character.isAlphabetic(c)){
+            if(Character.isLetterOrDigit(c)){
                 c = Character.toLowerCase(c);
                 sb.append(c);   
-                System.out.println(sb);
             }
-            else if(c == sp ){
-                continue;
-            }
-            //else if(Character.isDigit(c)){
-               // continue;
+           // else if(c == sp ){
+           //     continue;
            // }
+         
         }
         String org = sb.toString();
         String rev = sb.reverse().toString();
-        System.out.println(org + "  " + rev);
         if(org.equals(rev)){
             return true;
         }else{
