78dec008b621353741608d1b3272250fd0eda803
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-03-13 10:45:36 -0700

    Update 125fb.java

--- 125fb.java
+++ 125fb.java
@@ -24,3 +24,34 @@ class Solution {
         
     }
 }
+//another solution
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i); 
+            if(Character.isLetterOrDigit(c)){
+                c = Character.toLowerCase(c);
+                sb.append(c);   
+            }
+        }
+        String p = sb.toString();
+        return valid(p);
+        
+        
+    }
+    
+    public boolean valid(String s){
+        int i = 0;
+        int j = s.length()-1;
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
