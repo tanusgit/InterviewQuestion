541a36cc6eca25e6b859020a3d4a5ba83a8d3fa4
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-03-12 20:23:35 -0800

    Create Leetcode680.java

---
+++ Leetcode680.java
@@ -0,0 +1,50 @@
+class Solution{
+    
+    public boolean validPalindrome(String s) {
+       // HashMap<Boolean, Integer> map = new HashMap<>();
+       // boolean r = false;
+        for(int i =0; i < s.length(); i++){
+            String res = makestr(s, i);
+            boolean b = valid(res);
+            boolean c = valid(s);
+            if(b == true || c == true){
+                return true;
+            }
+          //  map.put(b, 0);
+        }
+        //System.out.print(map);
+      /*  if(map.containsKey(true)){
+            r = true;;
+        }
+        else{
+            r = false;
+        }
+        return r;*/
+        return false;
+    }
+    
+    public boolean valid(String s) {
+        int i = 0;
+        int j = s.length()-1;    
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+              return false;
+            }else{
+                i++;
+                j--;
+            }  
+        }
+        return true;
+    }
+    
+    public String makestr(String s, int k){
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i < s.length(); i++){
+            if(i == k){
+                continue;
+            }
+            sb.append(s.charAt(i)); 
+        }
+        return sb.toString();
+    }
+}
