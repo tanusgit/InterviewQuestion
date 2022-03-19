6d9e8d1991b56bd3d3fb4677d949b372584d5e26
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-03-19 00:14:21 -0700

    Update 88fb.java

--- 88fb.java
+++ 88fb.java
@@ -8,3 +8,42 @@ class Solution {
         Arrays.sort(nums1);
     }
 }
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        //copying the first array
+        int arr[] = new int[m];
+        for(int i =0; i < m; i++){
+            arr[i] = nums1[i];
+        }
+        
+        int p =0;
+        int j= 0;
+        int k = 0;
+        
+        while(p < m+n && k < m && j <n){
+            if(arr[k] < nums2[j]){
+                nums1[p] = arr[k];
+                p++;
+                k++;
+         
+            }else{
+                nums1[p] = nums2[j];
+                j++;
+                p++;
+            }
+        }
+        while(j < n){
+            nums1[p] = nums2[j];
+            j++;
+            p++;
+        }
+          while(k < m){
+            nums1[p] = arr[k];
+            k++;
+            p++;
+        }
+         
+    }
+}
