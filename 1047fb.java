e19a5d6f5116ae1b1006ec1a08c5080fc7faeed1
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-03-19 08:40:21 -0700

    Update 1047fb.java

--- 1047fb.java
+++ 1047fb.java
@@ -28,3 +28,23 @@ class Solution {
       
     }
 }
+//another solution
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for(int i =0; i< s.length(); i++){
+            char c = s.charAt(i);
+            if(!st.isEmpty() && st.peek() == c ){
+                st.pop();
+            }else{
+                st.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+           sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
