346114dff79ca4db25ebb7328fd376b36983da72
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-03-19 10:17:04 -0700

    Update 523fb.java

--- 523fb.java
+++ 523fb.java
@@ -62,3 +62,19 @@ class Solution {
     }
 }
 //passed 84/95 test cases
+//
+class Solution {
+   public boolean checkSubarraySum(int[] nums, int k) {
+        Map <Integer, Integer> map = new HashMap(); // key: rolling sum% k, val: array index
+        map.put(0, -1); 
+        int sum =0;
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i]; 
+            int mod = sum % k;  
+            if(map.containsKey(mod) && i-map.get(mod)>1) return true; 
+            map.putIfAbsent(mod,i);
+        }
+        return false;
+    }
+
+}
