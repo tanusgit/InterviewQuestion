4c56297d1ea309e2dbd6ac8e9f629bf1300f4072
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-03-30 07:58:55 -0700

    Update 1876SubstringsofSizeThreewithDistinctCharacters.java

--- 1876SubstringsofSizeThreewithDistinctCharacters.java
+++ 1876SubstringsofSizeThreewithDistinctCharacters.java
@@ -14,20 +14,7 @@ class Solution {
         }
         return count;
     }
-    //this function needs modification
-    public boolean isUnique(String s){
-        int i =0;
-        int j = s.length()-1;
-        while(i < j){
-            if(s.charAt(i) == s.charAt(j)){
-                return false;
-            }else{
-                i++;
-                j--;
-            }
-        }
-        return true;
-    }
+    
     
       public boolean isUnique2(String s){
         
