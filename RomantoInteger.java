4d408c9989172b475d9fd36194019632e4f3ef6e
Tanu D <24976766+tanusgit@users.noreply.github.com>
2022-04-17 09:59:06 -0700

    Update RomantoInteger.java

--- RomantoInteger.java
+++ RomantoInteger.java
@@ -1,65 +1,69 @@
 class Solution {
-    public int romanToInt(String s) {
-     
-            
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+    public int romanToInt(String s) {    
+        HashMap<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
         
         int res = 0;
         int num = 0;
         
         Stack<Character> st = new Stack<>();
         st.push(s.charAt(0));
-        for(int i =0; i < s.length(); i++){
+        for(int i = 1; i < s.length(); i++){
             char c = s.charAt(i);
-            if(!st.isEmpty() && c == 'L'){
-                if(st.peek() == 'X'){
-                    num = map.get(c) - map.get('X');
+            if(!st.isEmpty() && st.peek() == 'I' ){
+                if( c == 'V'){
+                    res = res + m.get(c) - m.get('I');
                     st.pop();
+                }
+                else if(c == 'X'){
+                    res = res + m.get(c) - m.get('I');
+                    st.pop();
+                }
+                else{
+                    st.push(c);
                 }
             }
-            else if(!st.isEmpty() && c == 'C'){
-                if(st.peek() == 'X'){
-                    num = map.get(c) - map.get('X');
+            else if(!st.isEmpty() && st.peek() == 'C' ){
+                if( c == 'D'){
+                    res = res + m.get(c) - m.get('C');
                     st.pop();
+                }
+                else if(c == 'M'){
+                    res = res + m.get(c) - m.get('C');
+                    st.pop();
+                }
+                else{
+                    st.push(c);
                 }
             }
-            else if(!st.isEmpty() && c == 'V'){
-                if(st.peek() == 'I'){
-                    num = map.get(c) - map.get('I');
+            else if(!st.isEmpty() && st.peek() == 'X' ){
+                if( c == 'L'){
+                    res = res + m.get(c) - m.get('X');
                     st.pop();
                 }
-            }     //CD = 400 CM = 900
-            else if(!st.isEmpty() && c == 'D'){
-                if(st.peek() == 'C'){
-                    num = map.get(c) - map.get('C');
+                else if(c == 'C'){
+                    res = res + m.get(c) - m.get('X');
                     st.pop();
+                    
                 }
-            }
-             else if(!st.isEmpty() && c == 'M'){
-                if(st.peek() == 'C'){
-                    num = map.get(c) - map.get('C');
-                    st.pop();
+                else{
+                    st.push(c);
                 }
             }
             else{
-                st.push(c);
+                  st.push(c);
             }
-            
-            res = res + num;
-            System.out.println("num = " + num + " res = " +res);
         }
-        System.out.println(st.toString());
         while(!st.isEmpty()){
-            res = res + map.get(st.pop());
+            num += m.get(st.pop());
         }
-  
-        return res;
+            num = num + res;
+        return num;
     }
 }
